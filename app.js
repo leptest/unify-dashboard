7bd058db4e9e130aade2c374b4df0c8356afb409
Daniel Chapman <daniel.chapman@eqtr.com>
2017-02-11 20:41:35 -0800

    formatting

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
- var express = require("express");
- var app = express();
+var express = require("express");
+var app = express();
 
- /* serves main page */
- app.get("/", function(req, res) {
- 	res.sendfile('index.html')
- });
+/* serves main page */
+app.get("/", function(req, res) {
+	res.sendfile('index.html')
+});
 
- /* serves all the static files */
- app.get(/^(.+)$/, function(req, res) {
- 	console.log('static file request : ' + req.params);
- 	res.sendfile(__dirname + req.params[0]);
- });
+/* serves all the static files */
+app.get(/^(.+)$/, function(req, res) {
+	console.log('static file request : ' + req.params);
+	res.sendfile(__dirname + req.params[0]);
+});
 
- var port = process.env.PORT || 80;
- app.listen(port, function() {
- 	console.log("Listening on " + port);
- });
+var port = process.env.PORT || 80;
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
